05681d7c61acb38ffac50ac74d7bb2450eef5825
kipkurui26 <6mutaibartai58@gmail.com>
2025-07-03 11:25:39 +0300

    [UPDATE] System Requirements

--- server/asgi.py
+++ server/asgi.py
@@ -1,24 +1,19 @@
-"""
-ASGI config for server project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-"""
-
 import os
-import django
 from django.core.asgi import get_asgi_application
 
-# Set the default Django settings module before any imports
+# Set default settings first
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
-django.setup()  # This is the key line that was missing
 
-# Now import other ASGI components
+# Get ASGI application
+django_application = get_asgi_application()
+
+# Now import other components
 from channels.routing import ProtocolTypeRouter, URLRouter
 from users.jwt_channels_middleware import JWTAuthMiddleware
 import users.routing
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_application,
     "websocket": JWTAuthMiddleware(
         URLRouter(
             users.routing.websocket_urlpatterns
